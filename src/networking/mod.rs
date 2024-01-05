49bd4a14f1939a31f3fef2f705e78b62577dd25c
James Mallon <me@jamesmallon.io>
2024-01-05 15:51:23 +0100

    fix(block-fetcher): remove error messages pertaining to block missing

--- src/networking/mod.rs
+++ src/networking/mod.rs
@@ -90,8 +90,8 @@ impl BlockFetcher for SolanaClient {
                 },
             )
             .map_err(|err| {
-                if err.to_string().contains("-32009") {
-                    // set verbosity lower as slot has been skipped
+                if err.to_string().contains("-32009") || err.to_string().contains("-32004") {
+                    // set verbosity lower as slot has been skipped or is not available
                     debug!("Could not retrieve block {} due to error: {}", slot, err)
                 } else {
                     error!("Could not retrieve block {} due to error: {}", slot, err);
